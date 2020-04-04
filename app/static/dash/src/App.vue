b6d62e0d164614266e2f359b453e1673141dd2a7
rqroz <rqroz1@gmail.com>
2020-04-04 10:44:55 -0300

    updated vuetify; set dark theme with base template;

--- app/static/dash/src/App.vue
+++ app/static/dash/src/App.vue
@@ -1,60 +1,41 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer app clipped :mini-variant="mini">
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Control Panel</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="mini = !mini" />
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container class="fill-height">
+        <router-view />
+      </v-container>
     </v-content>
+
+    <v-footer app>
+      <span>&copy; {{year}} Rodolfo Queiroz</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    data: () => ({
+      mini: true,
+      title: 'OBD II - Dashboard',
+      year: (new Date()).getFullYear(),
+    }),
+  }
 </script>
--- app/static/dash/vue.config.js
+++ app/static/dash/vue.config.js
@@ -3,7 +3,7 @@ const DEV_MODE = process.env.NODE_ENV === 'development';
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ],
+   ],
   publicPath: DEV_MODE ? 'http://localhost:8080/' : '/',
   outputDir: `../dist`,
   devServer: {
@@ -11,6 +11,6 @@ module.exports = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
       'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
-    }
+     }
   },
 };
