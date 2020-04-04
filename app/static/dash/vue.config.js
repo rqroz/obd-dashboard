67b722033887a5da1deff0480d6f9fc33fdcaa49
rqroz <rqroz1@gmail.com>
2020-04-04 10:14:19 -0300

    fixed integration with vue

--- app/static/dash/vue.config.js
+++ app/static/dash/vue.config.js
@@ -1,6 +1,16 @@
+const DEV_MODE = process.env.NODE_ENV === 'development';
+
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
+  transpileDependencies: [
+    'vuetify'
   ],
-  "outputDir": "../dist",
-}
+  publicPath: DEV_MODE ? 'http://localhost:8080/' : '/',
+  outputDir: `../dist`,
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
+  },
+};
--- app/views/index.py
+++ app/views/index.py
@@ -12,10 +12,9 @@ from app.constants import Environments
 class IndexViews:
     @classmethod
     def add_views(cls, server):
-        server.add_url_rule("/", defaults={'path': ''}, view_func=cls.index_view)
-        server.add_url_rule("/<path:path>", view_func=cls.index_view)
+        server.add_url_rule('/', view_func=cls.index_view, methods=("GET",))
 
-    def index_view(path):
+    def index_view():
         if Config.ENVIRONMENT == Environments.LOCAL:
-            return requests.get(f'http://localhost:8080/{path}').text
+            return requests.get('http://localhost:8080').text
         return render_template("index.html")
